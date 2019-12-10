9de5c57a8fb8cf8b2ed89ba25fec06c297d4baf5
Jazmin M <jazminmerckel@gmail.com>
2019-12-10 04:05:48 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "costume_inventory"
-    t.integer "employees_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
