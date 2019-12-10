8639abac27d9a08f733bf0fab5128fa37a5f94ac
Jazmin M <jazminmerckel@gmail.com>
2019-12-10 04:01:54 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme
       t.float :price
+      t.boolean :family_friendly
       t.date :opening_date
       t.date :closing_date
       t.text :description
